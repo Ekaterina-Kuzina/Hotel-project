23d27d73969eb6c02d779773306894865fc85370
Ekaterina Kuzina <Ekaterina_Kuzina@epam.com>
2022-04-07 16:33:38 +0300

    feat: add structure for project, fonts, common styles

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import appStyle from "./app.module.css";
+
+function App() {
+  return (
+    <div className={`${appStyle.app}`}>
+    Катя Кузина
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
